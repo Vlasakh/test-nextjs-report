1b47d41f73f94a9c613cada7c24c10a7ccce715a
Vladimir Sakharov <volodymyr.sakharov@exabeam.com>
2021-03-29 11:38:45 +0300

    changed navigation

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c448396cb65da1fbb68a";
+/******/ 	var hotCurrentHash = "07243bf5252557c78134";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
